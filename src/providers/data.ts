fc2f26c7f4024def453559295929a3e681120b39
ParthaPradeepNath <parthanath96134@gmail.com>
2026-02-20 17:15:19 +0530

    implement data provider

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,5 +1,35 @@
-import { createSimpleRestDataProvider } from "@refinedev/rest/simple-rest";
-import { API_URL } from "./constants";
-export const { dataProvider, kyInstance } = createSimpleRestDataProvider({
-  apiURL: API_URL,
-});
+// import { createSimpleRestDataProvider } from "@refinedev/rest/simple-rest";
+// import { API_URL } from "./constants";
+// export const { dataProvider, kyInstance } = createSimpleRestDataProvider({
+//   apiURL: API_URL,
+
+import { MOCK_SUBJECTS } from "@/constants/mock-data";
+import {
+  BaseRecord,
+  DataProvider,
+  GetListParams,
+  GetListResponse,
+} from "@refinedev/core";
+
+// });
+export const dataProvider: DataProvider = {
+  // In the parameter we get a getList functionality, it fetches all the records used by useTable, useList and so on. So, everytime we'll implement a list resource page, refine will make a call to get list to get the data and and pass it to the use table or use list behind the scene
+  getList: async <TData extends BaseRecord = BaseRecord>({
+    resource,
+  }: GetListParams): Promise<GetListResponse<TData>> => {
+    if (resource !== "subjects") {
+      return { data: [] as TData[],total: 0 }
+    }
+    return {
+      data: MOCK_SUBJECTS as unknown as TData[],
+      total: MOCK_SUBJECTS.length,
+    }
+  },
+  // this function fetches a single record by id
+  getOne: async () => {throw new Error('This function is not present in mock')},
+  create: async () => {throw new Error('This function is not present in mock')},
+  update: async () => {throw new Error('This function is not present in mock')},
+  deleteOne: async () => {throw new Error('This function is not present in mock')},
+
+  getApiUrl: () => ''
+}
